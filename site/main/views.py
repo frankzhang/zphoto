9ef1ebc14cb3a91416d8c0b2caa02e5bc16933ef
Frank Zhang <frankzhang.cn@gmail.com>
2018-12-12 18:41:33 +0000

    add file upload plugin main steps of image select->upload->process

--- site/main/views.py
+++ site/main/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse,HttpResponseNotAllowed
+from django import forms
+from django.core.files.storage import default_storage
+from django.core.files import File
+from pgmagick import Image, Blob, Geometry
 
 # Create your views here.
+
+def upload(request):
+    if request.method == 'POST':
+        print(request.POST)
+        print(request.FILES)
+
+        files = request.FILES.getlist('fileList[]')
+
+        for f in files:
+            # default_storage.save(None,f)
+            blob = Blob(f.read())
+            img = Image(blob,Geometry(100,100))
+            img .write(f.name)
+            print(f)
+
+        return JsonResponse({'msg':'SUCESS'})
+
+    return HttpResponseNotAllowed(permitted_methods=['POST'])
