30781f653c21d15a90c2df4c37dce63b8e997d22
Frank Zhang <frankzhang.cn@gmail.com>
2019-01-03 16:37:44 +0000

    1.send image file operation parameter to server as upload file extra data 2.get operation from request.POST and processing the uploaded file on server side.

--- site/main/views.py
+++ site/main/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse,JsonResponse,HttpResponseNotAllowed
 from django import forms
 from django.core.files.storage import default_storage
 from django.core.files import File
-from pgmagick import Image, Blob, Geometry
+from pgmagick import Image, Blob, Geometry, FilterTypes
 
 # Create your views here.
 
@@ -12,14 +12,57 @@ def upload(request):
         print(request.FILES)
 
         files = request.FILES.getlist('fileList')
-
+        
         for f in files:
-            # default_storage.save(None,f)
-            blob = Blob(f.read())
-            img = Image(blob,Geometry(100,100))
-            img .write(f.name)
             print(f)
+            if 'img_op' in request.POST:
+                processing(f,request.POST)
+            else:
+                default_storage.save(None,f)
 
         return JsonResponse({'msg':'SUCESS'})
 
     return HttpResponseNotAllowed(permitted_methods=['POST'])
+
+def processing(f, params):
+    operation = params['img_op']
+    blob = Blob(f.read())
+    img = Image(blob)
+
+    if operation == 'compress':
+        compress_level = params['compress_level']
+        depth = params['depth']
+        img.write(f.name)
+        
+        
+    elif operation == 'resize':
+        width = params['width']
+        height = params['height']
+        ignoreAspectRatio = params['ignoreAspectRatio']
+        
+        img.filterType(FilterTypes.SincFilter)
+        # img.resize(width+'x'+height)
+        img.scale(width+'x'+height)
+        img.write(f.name)
+        
+    elif operation == 'convert':
+        img_type = params['img_type'];
+        
+    elif operation == 'transparent':
+        pass
+    
+    elif operation == 'watermark':
+        watermarkText = params['ignoreAspectRatio']
+        opacity = params['opacity']
+        
+    elif operation == 'Animate':
+        
+        interval = params['interval']
+    else:
+        pass
+            
+        # blob = Blob(f.read())
+        # img = Image(blob,Geometry(100,100))
+        # img .write(f.name)
+        
+    return
